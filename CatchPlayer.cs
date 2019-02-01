230e89b03f1e3506d8f47ddbf2032c09cf52fa23
killersshadow <32833605+killersshadow@users.noreply.github.com>
2019-02-01 19:02:02 +0100

    Add files via upload

---
+++ CatchPlayer.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class CatchPlayer : MonoBehaviour
+{
+    [HideInInspector]
+    public bool IsAtPlayer = false;
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            IsAtPlayer = true;
+            Vector3 pos = new Vector3(GetComponent<MoveTowardsPos>().Startpos.x, transform.position.y, GetComponent<MoveTowardsPos>().Startpos.z);
+            transform.LookAt(pos);
+            
+        }
+    }
+}
